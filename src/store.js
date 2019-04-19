aaa45f27f67f034ff06a7df275eacbfdcfeb3bee
klack710 <willow710kut@gmail.com>
2019-04-20 03:29:04 +0900

    Vuexでフォームの値をaction経由で変更

--- src/store.js
+++ src/store.js
@@ -6,11 +6,35 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    message: '初期メッセージ'
+  },
+  getters: {
+    count(state) {
+      return state.count;
+    },
+    message(state) {
+      return state.message;
+    }
   },
   mutations: {
     increment(state) {
       state.count++;
+    },
+    mutationType(state, payload) {
+      state.count = payload;
+    },
+    setMessage(state, payload) {
+      state.message = payload.message;
+    }
+  },
+  actions: {
+    // store.dispatch('actionType', payload)で呼び出す
+    actionType({ commit }, payload) {
+      commit('mutationType');
+    },
+    doUpdate({ commit }, message) {
+      commit('setMessage', { message });
     }
   }
 });
